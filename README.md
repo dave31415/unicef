82203f02b88889e7800501740a1e3a1a4fe1c792
David Johnston <dave31415@gmail.com>
2015-07-01 16:07:14 -0400

    doesn't quite work yet

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # unicef
+
+Example
+
+from unicef import readers
--- unicef/readers.py
+++ unicef/readers.py
@@ -13,15 +13,16 @@ def file_names(name=None):
     return files
 
 
-def read_mics():
+def read_mics(verbose=False):
     mics_file = file_names('mics')
-    print mics_file
+    print 'reading file: %s' % mics_file
     wb = load_workbook(filename=mics_file)
     sheet_names = [i.title for i in wb.worksheets]
-    print '\n\tSheet names'
-    print line
-    for sheet_name in sheet_names:
-        print sheet_name
+    if verbose:
+        print '\n\tSheet names'
+        print line
+        for sheet_name in sheet_names:
+            print sheet_name
 
     return wb
 
@@ -32,24 +33,30 @@ def get_table_from_mics_file(table_name):
     return sheet.columns
 
 
-def parse_mics_breastfeeding():
+def parse_mics_breastfeeding(print_data=False, print_headers=False):
     table = get_table_from_mics_file('breastfeeding')
     primary_rows = table[0]
     secondary_rows = table[1]
     primary_columns = [column[0] for column in table]
     secondary_columns = [column[1] for column in table]
 
-    items = [primary_rows, secondary_rows, primary_columns, secondary_columns]
-    items_names = ['primary_rows', 'secondary_rows', 'primary_columns', 'secondary_columns']
+    if print_headers:
+        items = [primary_rows, secondary_rows, primary_columns, secondary_columns]
+        items_names = ['primary_rows', 'secondary_rows', 'primary_columns', 'secondary_columns']
 
-    for item_name, item in zip(items_names, items):
+        for item_name, item in zip(items_names, items):
 
-        print '\n\t %s' % item_name
+            print '\n\t %s' % item_name
+            print line
+            for cell in item:
+                value = cell.value
+                if value.strip():
+                    print value
+
+    if print_data:
         print line
-        for cell in item:
-            value = cell.value
-            if value.strip():
-                print value
+        print "\t Data"
+        print line
 
     # create a nested data structure
     # with four nested keys
@@ -84,10 +91,9 @@ def parse_mics_breastfeeding():
             if secondary_col_this:
                 secondary_col = secondary_col_this
 
-
-            print primary_row, secondary_row, primary_col, secondary_col, value
-
             if primary_row and secondary_row and primary_col and secondary_col:
                 data[primary_row][secondary_row][primary_col][secondary_col] = value
+                if print_data:
+                    print "%s, %s, %s, %s, value: %s" % (primary_row, secondary_row, primary_col, secondary_col, value)
 
     return data
