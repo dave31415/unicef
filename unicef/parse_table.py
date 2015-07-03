d1fc32f6a3f6ada6df6ccd075e75032eaa56ebcd
David Johnston <dave31415@gmail.com>
2015-07-03 14:18:07 -0400

    more utilities added to HTable class printing better indexing

--- unicef/parse_table.py
+++ unicef/parse_table.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 
 def print_cell(cell):
@@ -24,6 +24,7 @@ def get_unmerged_table_columns(table):
         if cell.value.strip() and not cell.merged:
             columns.append(cell)
             column_index.append(i)
+
     return columns, column_index
 
 
@@ -48,10 +49,8 @@ def get_rc_structure(rc, rc_index, n_rc_all):
 def get_row_col_structure(table):
     rows, row_index = get_unmerged_table_rows(table)
     columns, column_index = get_unmerged_table_columns(table)
-
     n_columns_all = len(table)
     n_rows_all = len(table[0])
-
     row_lookup = get_rc_structure(rows, row_index, n_rows_all)
     column_lookup = get_rc_structure(columns, column_index, n_columns_all)
     return row_lookup, column_lookup
@@ -62,6 +61,7 @@ class HTable():
     def __init__(self, table):
         self.row_lookup, self.column_lookup = get_row_col_structure(table)
         self.table = table
+        self.verbose = True
 
     def row_names(self):
         return self.row_lookup.keys()
@@ -70,6 +70,20 @@ class HTable():
         return self.column_lookup.keys()
 
     def _index(self, row_name, column_name):
+        # allow integer indexing as well
+        # rather than by name (less typing)
+        if isinstance(row_name, int):
+            row_name = self.row_names()[row_name]
+        if isinstance(column_name, int):
+            column_name = self.column_names()[column_name]
+
+        if self.verbose:
+            print '\n-------------------------------------------'
+            print 'selected'
+            print "Row name:     %s" % row_name
+            print "Column name:  %s" % column_name
+            print '-------------------------------------------\n'
+
         if row_name not in self.row_names():
             return None
         if column_name not in self.column_names():
@@ -122,13 +136,62 @@ class HTable():
                 row.append(column[row_index])
             sub_table.append(row)
 
-        return HTable(sub_table)
+        # Trick to make it work when there is either
+        # a single column or row
+        # Gives that single row or column
+        # a name "." which keeps it an HTable
+        # I *think* this works, needs more tests
+
+        if len(columns_to_keep) == 2:
+            sub_table[1][0].value = '.'
+            sub_table[1][0].merged = False
+
+        if len(rows_to_keep) == 2:
+            sub_table[0][1].value = '.'
+            sub_table[0][1].merged = False
+
+        ret_value = HTable(sub_table)
+        n_row = len(ret_value.row_names())
+        n_col = len(ret_value.column_names())
+        if self.verbose:
+            dims = (n_row, n_col)
+            print '\nReturning another HTable with %s rows and %s columns' % dims
+        return ret_value
 
     def __call__(self, *args, **kwargs):
         # Makes the object callable without having to
         # use the _index() method
          return self._index(*args, **kwargs)
 
+    def _info(self):
+        short_line = '--------------'
+        row_names = [str(i)+' - '+j for i,j in enumerate(self.row_names())]
+        col_names = [str(i)+' - '+j for i,j in enumerate(self.column_names())]
+        row_line = "\nRows: \n%s\n%s" % (short_line, "\n".join(row_names))
+        col_line = "\n\nColumns: \n%s\n%s" % (short_line, "\n".join(col_names))
+        return row_line+col_line
 
+    def __repr__(self):
+        return self._info()
 
+    def _transpose(self):
+        # rearranges columns of rows to rows of columns
+        # and takes the 'value'
+        # only used for printing
+        trans_table = defaultdict(list)
+        for column in self.table:
+            for row_num, cell in enumerate(column):
+                trans_table[row_num].append(cell.value)
+        return [kv[1] for kv in sorted(trans_table.items())]
 
+    def _print(self):
+        # pretty printing the table
+        # could be improved
+        str_length = 15
+        just_length = str_length+1
+        for row in self._transpose():
+            row_string = [str(i)[0:str_length].ljust(just_length) for i in row]
+            print ' | '.join(row_string)
+
+    def p(self):
+        self._print()
