5fdf69b344e1524b58bd40a521dba81e4d6baba3
Sara-Jayne Terp <sjterp@sjterp.local>
2015-07-07 17:32:06 -0400

    more adventures with mongodb

--- unicef/add_countries.py
+++ unicef/add_countries.py
@@ -1,8 +1,20 @@
 from __future__ import print_function
 from collections import OrderedDict, defaultdict
-import pymongo
+from pymongo import MongoClient
 import csv
 from params import pemfile, awsinstance
+import ssl
+
+#Open Mongo instance
+#Authentication: http://api.mongodb.org/python/current/examples/authentication.html
+
+client = MongoClient(awsinstance,
+	ssl=True,
+	ssl_certfile=pemfile)
+
+client.the_database.authenticate("<X.509 derived username>",
+	mechanism='MONGODB-X509')
+db = client.unicef
 
 #Push data to Mongdb instance
 infile = "iso3166alpha3.csv"
@@ -11,4 +23,5 @@ csvin = csv.reader(fin, dialect=csv.excel_tab)
 headers = csvin.next()
 for row in csvin:
 	print("{}".format(row))
+
 
